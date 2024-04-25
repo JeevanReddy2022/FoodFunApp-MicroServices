21ba90061fc91b6ea9d048df9fca3738c57b9d62
JeevanReddy2022 <jeevanreddy4372@gmail.com>
2024-04-25 12:42:19 +0530

    final commit

---
+++ Back-End/Userservice/src/main/java/com/Userservice/DTO/LoginResponseDTO.java
@@ -0,0 +1,17 @@
+package com.Userservice.DTO;
+
+
+import lombok.Data;
+@Data
+public class LoginResponseDTO {
+
+
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+
+    private Integer foodCartId;
+	
+}
---
+++ Back-End/Userservice/src/main/java/com/Userservice/DTO/RegistrationDTO.java
@@ -0,0 +1,18 @@
+package com.Userservice.DTO;
+
+
+
+import lombok.Data;
+
+@Data
+public class RegistrationDTO {
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String password;
+
+    private Integer foodCartId;
+
+}
